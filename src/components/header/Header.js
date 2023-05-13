6f27045729db9488aec374a7a89b7f61e58b3fe4
Harshit Pachori <harshitpachori345@gmail.com>
2023-05-13 19:45:31 +0530

    updated readme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,12 +13,24 @@ function Header() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-            <Nav className="me-auto my-2 my-lg-0" style={{maxHeight:'100px'}} navbarScroll>
-<NavLink className="nav-link" to="/">Home</NavLink>
-<NavLink className="nav-link" to="/watchList">Watch List</NavLink>
-            </Nav>
-            <Button className="me-2" variant="outline-info">Login</Button>
-            <Button className="me-2" variant="outline-info">Register</Button>
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/watchList">
+              Watch List
+            </NavLink>
+          </Nav>
+          <Button className="me-2" variant="outline-info">
+            Login
+          </Button>
+          <Button className="me-2" variant="outline-info">
+            Register
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
